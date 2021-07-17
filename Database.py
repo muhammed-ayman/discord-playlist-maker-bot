b49c2c4b04408c8a18e12d73d9f2fe0f622de439
Mohammed ayman <mae252002@gmail.com>
2021-07-17 21:58:05 +0200

    added list methods & replaced playlist and songs tables with env vars

--- Database.py
+++ Database.py
@@ -15,7 +15,7 @@ class Database:
     # CHECK IF PLAYLIST EXISTS
     def check_playlist(self, playlist_name):
         sql_query = f"""
-            SELECT * FROM playlists WHERE playlist = '{playlist_name}'
+            SELECT * FROM {os.getenv('PLAYLISTS_TABLE')} WHERE playlist = '{playlist_name}'
         """
         self.cursor.execute(sql_query)
         if self.cursor.fetchone() != None:
@@ -26,7 +26,7 @@ class Database:
     def insert_playlist(self,playlist):
         if self.check_playlist(playlist):
             return
-        sql_query = f"INSERT INTO playlists (playlist) VALUES ('{playlist}')"
+        sql_query = f"INSERT INTO {os.getenv('PLAYLISTS_TABLE')} (playlist) VALUES ('{playlist}')"
         self.cursor.execute(sql_query)
         self.connection.commit()
     # INSERT A SONG IN THE GIVEN PLAYLIST IF EXISTS OR CREATE IT AND INSERT AFTERWARDS
@@ -34,7 +34,24 @@ class Database:
         if not self.check_playlist(playlist):
             self.insert_playlist(playlist)
         sql_query = f"""
-            INSERT INTO songs (song,playlist,link) VALUES ('{song}','{playlist}','{link}')
+            INSERT INTO {os.getenv('SONGS_TABLE')} (song,playlist,link) VALUES ('{song}','{playlist}','{link}')
         """
         self.cursor.execute(sql_query)
         self.connection.commit()
+    # LIST AVAILABLE PLAYLISTS IN THE DB
+    def list_playlists(self):
+        sql_query = f"SELECT * FROM {os.getenv('PLAYLISTS_TABLE')}"
+        self.cursor.execute(sql_query)
+        playlists_data = self.cursor.fetchall()
+        return playlists_data
+    # LIST DATA IN A GIVEN PLAYLIST
+    def list_playlist(self, playlist_name):
+        sql_query = f"""
+            SELECT song, link FROM {os.getenv('SONGS_TABLE')} WHERE playlist = '{playlist_name}'
+        """
+        if self.check_playlist(playlist_name):
+            self.cursor.execute(sql_query)
+            playlist_data = self.cursor.fetchall()
+            return playlist_data
+        else:
+            return None
